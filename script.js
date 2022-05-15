113457dbcafd021fbb6d67519670c0b69e4a6359
James Nyamu <nyamuj59@gmail.com>
2022-05-15 13:49:33 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const search = document.querySelector(".search");
+const input = document.querySelector(".input");
+const button = document.querySelector(".btn");
+
+
+button.addEventListener("click", () => {
+    search.classList.toggle("active");
+    input.focus();
+});
